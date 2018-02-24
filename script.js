981c807d72ba3c74851a765265f1ebfc6c7102d9
Kristyn Ginski <missginski@gmail.com>
2018-02-23 21:13:24 -0500

    pass objects to dom

--- script.js
+++ script.js
@@ -5,51 +5,58 @@ function parseRSS(url) {
 		method: 'GET',
 		dataType: 'json',
 		url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url) + '&api_key=' + api_key,
-		success: function(data){
-			if (data.status != 'ok'){
-				throw data.message;
+		success: function(response){
+			if (response.status != 'ok'){
+				throw response.message;
 			}
-			getArticles(data)
-			// console.log(data)
+			getArticles(response)
 		}
 	});
 }
 
-getArticles = function(data) {
-	let articles = data.items;
-	let myData = {};
+parseRSS('https://feeds.theguardian.com/theguardian/uk/rss');
+
+getArticles = function(response) {
+	let articles = response.items;
 	let myArticles = [];
-
-
-	$.each(articles, function(i, article){
-		for (var i in articles){
-			var item = articles[i];
-			myData.title = item.title;
-			myData.link = item.link;
-			myData.pubDate = item.pubDate;
-			myData.img = item.enclosure.link;
-			myData.desc = item.content;
-			myData.thumbnail = item.thumbnail;
-		}
-		let myArticle = myData[i];
-		myArticles.push(myArticle);
-
-	})
+	function NewData(newTitle, newLink, newDate, newImg, newDesc, newThumbnail){
+		this.title = newTitle;
+		this.link = newLink;
+		this.pubDate = newDate;
+		this.img = newImg;
+		this.desc = newDesc;
+		this.thumbnail = newThumbnail;
+	}
+	for (let i = 0; i <= 3; i++){
+		let article = articles[i];
+		let myData = new NewData(article.title, article.link, article.pubDate, article.enclosure.link, article.content, article.thumbnail);
+		myArticles.push(myData);
+	}
 	showArticles(myArticles)
 }
 
+showArticles = function(myArticles){
+	// console.log(myArticles)
+	let carImg = $('.carousel-item img');
+	let carTitle = $('.carousel-item a');
+	let carSource = $('.carousel-item p');
 
+	$.each(myArticles, function(i, eachArticle){
+		// console.log(eachArticle)
+		let title = eachArticle.title;
+		let link = eachArticle.link;
+		let img = eachArticle.img;
 
-showArticles = function(myData){
-	console.log(myData);
+		$('carImg').each(function(i){
+			$('carImg').attr('src', img)
+			console.log(i)
+		})
+
+		// listTitle.text(title);
+		// listLink.attr('src', link);
+		// listImg.attr('src', img);
+		// console.log(title, link, img)
+	})
+
+	// console.log(title, link, img)
 }
-
-parseRSS('https://www.vice.com/en_us/rss/');
-
-
-
-
-
-
-
-// select the html elements that will use them and append
