b1c0cb38a5ccdd9a729b6745e7c9f2793412c59b
Kristyn Ginski <missginski@gmail.com>
2018-03-09 12:35:23 -0500

    handle muliple json responses and templates

--- script.js
+++ script.js
@@ -1,5 +1,80 @@
 const api_key = config.API_KEY;
 
+function renderAsideStories() {
+	let url = 'http://feeds.feedburner.com/TechCrunch/';
+	$.ajax({
+		method: 'GET',
+		dataType: 'json',
+		url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url) + '&api_key=' + api_key,
+		success: function(response){
+			let articles = response.items,
+					myArticles = [];
+			$.each(articles, function(i, eachArticle){
+				myArticles.push(eachArticle);
+				if (i === 2){
+					return false;
+				}
+				// console.log(myArticles)
+				let template = $('#aside-tmpl').html();
+				let rendered = Mustache.render(template, myArticles);
+				$('#aside').html(rendered);
+			})
+		}
+	})
+}
+renderAsideStories()
+
+function renderLatestStories() {
+	let url = 'https://www.theverge.com/rss/frontpage';
+	$.ajax({
+		method: 'GET',
+		dataType: 'json',
+		url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url) + '&api_key=' + api_key,
+		success: function(response){
+			let articles = response.items,
+					myArticles = [];
+			$.each(articles, function(i, eachArticle){
+				myArticles.push(eachArticle);
+				if (i === 3){
+					return false;
+				}
+				// console.log(myArticles)
+				let template = $('#latest-tmpl').html();
+				let rendered = Mustache.render(template, myArticles);
+				$('#latest').html(rendered);
+			})
+		}
+	})
+}
+renderLatestStories()
+
+function renderPopularStories() {
+	let url = 'http://feeds.mashable.com/Mashable';
+	$.ajax({
+		method: 'GET',
+		dataType: 'json',
+		url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url) + '&api_key=' + api_key,
+		success: function(response){
+			let articles = response.items,
+					myArticles = [];
+			$.each(articles, function(i, eachArticle){
+				myArticles.push(eachArticle);
+				if (i === 6){
+					return false;
+				}
+				console.log(myArticles)
+				let template = $('#popular-tmpl').html();
+				let rendered = Mustache.render(template, myArticles);
+				$('#popular-stories').html(rendered);
+			})
+		}
+	})
+}
+renderPopularStories()
+
+
+
+
 function parseRSS(url, ii) {
 	$.ajax({
 		method: 'GET',
@@ -10,13 +85,12 @@ function parseRSS(url, ii) {
 				throw response.message;
 			}
 			handleResponse(response, ii)
-			return response
+			// return response
 		}
 	});
 }
 
-
-handleResponse = function(response, ii) {
+function handleResponse(response, ii) {
 	let articles = response.items;
 	let myArticles = [];
 	$.each(articles, function(i, eachArticle){
@@ -25,7 +99,7 @@ handleResponse = function(response, ii) {
 			return false;
 		}
 		showTopStories(myArticles)
-		return myArticles
+		// return myArticles
 	})
 }
 
@@ -38,44 +112,4 @@ function showTopStories(myArticles) {
 
 function renderFeeds() {
 	let asideStories = parseRSS('http://nypost.com/tech/feed/', 2);
-	console.log(asideStories)
 }
-
-renderFeeds()
-
-
-
-
-
-
-
-
-
-// function NewData(newTitle, newLink, newDate, newImg, newDesc, newThumbnail){
-// 	this.title = newTitle;
-// 	this.link = newLink;
-// 	this.pubDate = newDate;
-// 	this.img = newImg;
-// 	this.desc = newDesc;
-// 	this.thumbnail = newThumbnail;
-// }
-
-// let myData = new NewData(eachArticle.title, eachArticle.link, eachArticle.pubDate, eachArticle.enclosure.link, eachArticle.content, eachArticle.thumbnail);
-
-// console.log(JSON.stringify(obj));
-
-
-var obj = {
-	 articles : [
-		{
-			title: "Jeff Zucker joins fight to monetize mobile journalism",
-			link: "https://nypost.com/2018/02/26/jeff-zucker-joins-fight-to-monetize-mobile-journalism/",
-			img: "http://thenypost.files.wordpress.com/2018/02/afp_11a95l.jpg?quality=90&amp;strip=all"
-		},
-		{
-			title: "Ford to test self-driving pizza delivery with Domino’s",
-			link: "https://nypost.com/2018/02/27/ford-to-test-self-driving-pizza-delivery-with-dominos/",
-			img: "http://thenypost.files.wordpress.com/2018/02/ford-self-driving-car.jpg?quality=90&amp;strip=all"
-		}
-	]
-};
