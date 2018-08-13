45249958a872010d15345367cc5814694cf13a45
Kristyn Ginski <missginski@gmail.com>
2018-08-13 13:48:15 -0400

    abstract helper function

--- js/script.js
+++ js/script.js
@@ -4,82 +4,56 @@ const asideFeed = 'https://thoughtcatalog.com/feed/';
 const latestFeed = 'https://tinybuddha.com/feed/';
 const popularFeed = 'http://www.byrdie.com/rss';
 
-// get articles
-function getData(feedurl, handler) {
+
+function getData(feedurl, handler, val) {
 	$.ajax({
 		method: 'GET',
 		dataType: 'json',
 		url: 'https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(feedurl) + '&api_key=' + api_key,
 		success: function(response) {
-			handler(response)
+			let articles = response.items;
+			let myArticles = [];
+			$.each(articles, function(i, article) {
+				myArticles.push(article);
+				if ( i === val ) {
+					return false;
+				}
+			})
+			handler(myArticles)
 		}
 	})
 }
-getData(carouselFeed, renderCarouselStories);
-getData(asideFeed, renderAsideStories);
-getData(latestFeed, renderLatestStories);
-getData(popularFeed, renderPopularStories);
+getData(carouselFeed, renderCarouselStories, 2);
+getData(asideFeed, renderAsideStories, 1);
+getData(latestFeed, renderLatestStories, 2);
+getData(popularFeed, renderPopularStories, 8);
+
 
 // render articles
-function renderCarouselStories(response) {
-	let articles = response.items;
-	let myArticles = [];
-
-	$.each(articles, function(i, article) {
-		myArticles.push(article);
-		if ( i === 3 ) {
-			return false;
-		}
-	})
-
+function renderCarouselStories(myArticles) {
 	let template = $('#carousel-tmpl').html();
 	let rendered = Mustache.render(template, myArticles);
 	$('#carousel').html(rendered);
 	$('.carousel-item:first-child').addClass('active');
 }
 
-function renderAsideStories(response) {
-	let articles = response.items;
-	let myArticles = [];
 
-	$.each(articles, function(i, article) {
-		myArticles.push(article);
-		if ( i === 2 ) {
-			return false;
-		}
-
-		let template = $('#aside-tmpl').html();
-		let rendered = Mustache.render(template, myArticles);
-		$('#aside').html(rendered);
-	})
+function renderAsideStories(myArticles) {
+	let template = $('#aside-tmpl').html();
+	let rendered = Mustache.render(template, myArticles);
+	$('#aside').html(rendered);
 }
 
-function renderLatestStories(response) {
-	let articles = response.items;
-	let myArticles = [];
 
-	$.each(articles, function(i, article) {
-		myArticles.push(article);
-		if ( i === 3 ) {
-			return false;
-		}
-		let template = $('#latest-tmpl').html();
-		let rendered = Mustache.render(template, myArticles);
-		$('#latest').html(rendered);
-	})
+function renderLatestStories(myArticles) {
+	let template = $('#latest-tmpl').html();
+	let rendered = Mustache.render(template, myArticles);
+	$('#latest').html(rendered);
 }
 
-function renderPopularStories(response) {
-	let articles = response.items;
-	let myArticles = [];
 
-	$.each(articles, function(i, article) {
-		myArticles.push(article);
-		if ( i === 3 ) {
-			return false;
-		}
-		let template = $('#popular-tmpl').html();
-		let rendered = Mustache.render(template, myArticles);
-		$('#popular-stories').html(rendered);
-	})
+function renderPopularStories(myArticles) {
+	let template = $('#popular-tmpl').html();
+	let rendered = Mustache.render(template, myArticles);
+	$('#popular-stories').html(rendered);
 }
